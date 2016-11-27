d05601553b9b290387c69ad2ae613d2d27ce751d
Jonny Burch <jonny@jonnyburch.com>
2016-11-27 14:31:15 +0000

    more stuff

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ $(document).ready(function(){
   // Target your .container, .wrapper, .post, etc.
   $(".container").fitVids();
 });
+
+// Scroll to
+
+$(".scroll").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#elementtoScrollToID").offset().top
+    }, 2000);
+});
